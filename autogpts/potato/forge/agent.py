917f3332706f35e38d023fbcacfaf5aff68c4eb1
serg-v <serg-v@yandex-team.ru>
2023-10-19 06:08:53 +0200

    sort csv debug 3, also try gpt 4

--- autogpts/potato/forge/agent.py
+++ autogpts/potato/forge/agent.py
@@ -16,7 +16,8 @@ from forge.sdk import (
 
 LOG = ForgeLogger(__name__)
 
-MODEL = "gpt-3.5-turbo-0613"
+#MODEL = "gpt-3.5-turbo-0613"
+MODEL = "gpt-4-0613"
 
 class ForgeAgent(Agent):
     """
@@ -94,55 +95,6 @@ class ForgeAgent(Agent):
             f"📦 Task created: {task.task_id} input: {task.input[:40]}{'...' if len(task.input) > 40 else ''}"
         )
         return task
-
-    async def execute_step_clone(self, task_id: str, step_request: StepRequestBody) -> Step:
-        """
-        For a tutorial on how to add your own logic please see the offical tutorial series:
-        https://aiedge.medium.com/autogpt-forge-e3de53cc58ec
-
-        The agent protocol, which is the core of the Forge, works by creating a task and then
-        executing steps for that task. This method is called when the agent is asked to execute
-        a step.
-
-        The task that is created contains an input string, for the bechmarks this is the task
-        the agent has been asked to solve and additional input, which is a dictionary and
-        could contain anything.
-
-        If you want to get the task use:
-
-        ```
-        task = await self.db.get_task(task_id)
-        ```
-
-        The step request body is essentailly the same as the task request and contains an input
-        string, for the bechmarks this is the task the agent has been asked to solve and
-        additional input, which is a dictionary and could contain anything.
-
-        You need to implement logic that will take in this step input and output the completed step
-        as a step object. You can do everything in a single step or you can break it down into
-        multiple steps. Returning a request to continue in the step output, the user can then decide
-        if they want the agent to continue or not.
-        """
-        # An example that
-        step = await self.db.create_step(
-            task_id=task_id, input=step_request, is_last=True
-        )
-
-        self.workspace.write(task_id=task_id, path="output.txt", data=b"Washington D.C")
-
-        await self.db.create_artifact(
-            task_id=task_id,
-            step_id=step.step_id,
-            file_name="output.txt",
-            relative_path="",
-            agent_created=True,
-        )
-
-        step.output = "Washington D.C"
-
-        LOG.info(f"\t✅ Final Step completed: {step.step_id}")
-
-        return step
 
     async def execute_step(self, task_id: str, step_request: StepRequestBody) -> Step:
 
